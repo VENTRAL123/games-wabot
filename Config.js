d0b83205e794d91394e2b5ca126c933d129d2cac
VENTRAL123 <94768392+VENTRAL123@users.noreply.github.com>
2021-11-21 11:35:55 +0700

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,61 @@
+global.DeveloperMode = 'false' //true Or false
+global.linkGC = ['wa.me/6281575651838']
+global.Owner = {
+  // use the phone number with the country code, for example Indonesia '62' America '1' etc
+  '6281575651838': { // put your number here
+    name: 'ANGGA BOTZ',
+    isDev: false, // if true this number will send if bot have bug and developer mode true
+    isReport: false, // if true this number will send if user use command /report or /join
+    isCreator: true // if true this number will send if user use command /owner or /creator
+  },
+  '6281575651838': { // put your number here
+    isDev: true, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: false // if true this number will send if user use command /owner or /creator
+  },
+  '6281575651838': { // put your number here
+    name: 'ANGGA BOTZ',
+    isDev: false, // if true this number will send if bot have bug and developer mode true
+    isReport: true, // if true this number will send if user use command /report or /join
+    isCreator: false // if true this number will send if user use command /owner or /creator
+  },
+  '6281575651838': {
+    name: 'ANGGA BOTZ',
+    isReport: true, // if true this number will send if user use command /report or /join
+  },
+  '6281575651838': {
+    isReport: true
+  },
+  '6281575651838': {} // ignore if you don't want anything, like name dll
+}
+global.mods = ['6281575651838'] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  hardianto: 'http://hardianto-chan.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'http://hardianto-chan.herokuapp.com': 'hardianto'
+}
+
+// Sticker WM
+global.packname = 'wa.me/6281575651838'
+global.author = 'ANGGA BOTZ'
+
+global.multiplier = 39 // The higher, The harder levelup
+
+let fs = require('fs')
